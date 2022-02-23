898e50fb4c4c7524736376e42ef509198f5a93e4
akmo90 <100136978+akmo90@users.noreply.github.com>
2022-02-23 10:58:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,22 @@ day2:"Fri-Sun:",
       });
     });
 
+
+//scroll
+    $(document).ready(function(){
+      
+          $("#menu").on("click","a", function (event) {
+      
+              event.preventDefault(); 
+      
+              var id  = $(this).attr('href'),
+      
+                  top = $(id).offset().top;
+      
+              $('body,html').animate({scrollTop: top}, 1200);
+          });  
+      });
+
     $(document).ready(function(){
       $("#hide").click(function(){
         $("h5").hide();
